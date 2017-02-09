11427a5e5d2a63218d9daf5c02cb3da5ff45687a
zhengwei931102 <admin@admindeMac-mini.local>
2017-02-09 08:43:46 +0800

    NSOpenPanel NSSavePanel

--- NSOpenPanel/AppDelegate.m
+++ NSOpenPanel/AppDelegate.m
@@ -14,6 +14,53 @@
 @end
 
 @implementation AppDelegate
+- (IBAction)panel:(id)sender {
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    [panel setCanChooseDirectories:YES];
+    [panel setDirectoryURL:[NSURL fileURLWithPath:[NSString stringWithFormat:@"/Users/%@/Documents",NSUserName()]]];
+    [panel setCanChooseFiles:YES];
+    [panel setAllowedFileTypes:@[@"mov",@"mp4"]];
+    [panel setAllowsMultipleSelection:NO];//多选
+    [panel setMessage:@"Please select video to show."];
+    if ([panel runModal] == NSModalResponseOK) {
+        NSString *path = [panel.URLs.firstObject path];
+        NSLog(@"%@",path);
+    }
+}
+- (IBAction)panel2:(id)sender {
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    [panel setCanChooseDirectories:YES];
+    [panel setCanChooseFiles:NO];
+    panel.prompt = @"Choose";
+    [panel setMessage:@"Please select a directory to show."];
+    if ([panel runModal] == NSModalResponseOK) {
+        NSString *path = [panel.URLs.firstObject path];
+        NSLog(@"%@",path);
+        //code
+    }
+}
+- (IBAction)savePanel:(id)sender {
+    NSSavePanel*    panel = [NSSavePanel savePanel];
+    [panel setNameFieldStringValue:@"Untitle.iso"];
+    [panel setMessage:@"Choose the path to save the document"];
+    [panel setAllowsOtherFileTypes:YES];
+    [panel setAllowedFileTypes:@[@"iso"]];
+    [panel setDirectoryURL:[NSURL fileURLWithPath:[NSString stringWithFormat:@"/Users/%@/Documents",NSUserName()]]];
+    [panel setExtensionHidden:YES];//后缀名，例如：@“iso”
+    [panel setCanCreateDirectories:YES];
+    [panel beginSheetModalForWindow:self.window completionHandler:^(NSInteger result){
+        if (result == NSFileHandlingPanelOKButton)
+        {
+            
+            NSString *path = [[panel URL] path];
+            NSLog(@"%@______",path);
+            [@"iso" writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:nil];
+        }
+        
+    }];
+    
+
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
